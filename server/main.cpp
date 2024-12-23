aea8ba080d94730a815097b549363c17adbaf1c0
Fratee <elia.frate@gmail.com>
2024-12-23 16:07:42 +0100

    read server port from ini file

--- server/main.cpp
+++ server/main.cpp
@@ -1,14 +1,21 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include "tcpserver.h"
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    // Create a QSettings object to read from the config file
+    QSettings settings("config.ini", QSettings::IniFormat);
+
+    // Read the port from the config file (default to 12744 if not found)
+    int port = settings.value("Network/port", 12744).toInt();
+
     // Create and start the server
     TcpServer server;
-    if (!server.startServer(12744)) {
+    if (!server.startServer(port)) {
         qDebug() << "Failed to start the server!";
         return -1;
     }
